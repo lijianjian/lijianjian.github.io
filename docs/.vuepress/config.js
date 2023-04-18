996a85992c88450bbd97c30e3ee69987a4f8e77c
Shawn Li <shawn.li@viljasolutions.com>
2023-04-18 09:33:37 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,27 @@
+import { defineUserConfig } from 'vuepress'
+import { defaultTheme } from '@vuepress/theme-default'
+
+export default {
+    theme: defaultTheme({
+        logo: 'https://vuejs.org/images/logo.png',
+      navbar: [
+        // NavbarItem
+        {
+          text: 'Foo',
+          link: '/foo/',
+        },
+        // NavbarGroup
+        {
+          text: 'Group',
+          children: ['/group/foo.md', '/group/bar.md'],
+        },
+        // 字符串 - 页面文件路径
+        '/bar/README.md',
+      ],
+    }),
+  }
+// export default defineUserConfig({
+//   lang: 'zh-CN',
+//   title: '你好， VuePress ！',
+//   description: '这是我的第一个 VuePress 站点',
+// })
